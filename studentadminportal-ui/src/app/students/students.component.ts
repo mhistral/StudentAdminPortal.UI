bd615dc703c165908ddb5aee484fd2b74a968a16
Kimmy <kimberlyparrenas@gmail.com>
2022-07-09 14:00:26 +0800

    Implementing Sort

--- studentadminportal-ui/src/app/students/students.component.ts
+++ studentadminportal-ui/src/app/students/students.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Student } from '../models/ui.models/student.model';
 import { StudentService } from './student.service';
@@ -16,6 +17,7 @@ export class StudentsComponent implements OnInit {
   dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>();
 
   @ViewChild(MatPaginator) matPaginator!: MatPaginator;
+  @ViewChild(MatSort) MatSort!: MatSort; // of type
 
   constructor(private studentsService: StudentService) { }
 
@@ -29,6 +31,10 @@ export class StudentsComponent implements OnInit {
 
         if(this.matPaginator){
           this.dataSource.paginator =  this.matPaginator;
+        }
+
+        if(this.MatSort){
+          this.dataSource.sort = this.MatSort;
         }
       },
       (errorResponse) => {
