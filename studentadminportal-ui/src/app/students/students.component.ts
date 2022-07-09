e108c30305eef1d76a30cb2694c1a3ba93c6d457
Kimmy <kimberlyparrenas@gmail.com>
2022-07-09 13:55:18 +0800

    Implementing paginator

--- studentadminportal-ui/src/app/students/students.component.ts
+++ studentadminportal-ui/src/app/students/students.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Student } from '../models/ui.models/student.model';
 import { StudentService } from './student.service';
@@ -14,6 +15,8 @@ export class StudentsComponent implements OnInit {
   displayedColumns: string[] = ['firstName', 'lastName', 'dateOfBirth', 'email', 'mobile', 'gender'];
   dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>();
 
+  @ViewChild(MatPaginator) matPaginator!: MatPaginator;
+
   constructor(private studentsService: StudentService) { }
 
   ngOnInit(): void {
@@ -23,6 +26,10 @@ export class StudentsComponent implements OnInit {
       (successResponse) => {
         this.students = successResponse;
         this.dataSource = new MatTableDataSource<Student>(this.students);
+
+        if(this.matPaginator){
+          this.dataSource.paginator =  this.matPaginator;
+        }
       },
       (errorResponse) => {
         console.log(errorResponse)
