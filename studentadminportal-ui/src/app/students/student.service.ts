bc61931f945ed8544ff6aa768d7894eced87a6e3
Kimmy <kimberlyparrenas@gmail.com>
2022-07-24 15:03:21 +0800

    Creating Student Service Method in UI

--- studentadminportal-ui/src/app/students/student.service.ts
+++ studentadminportal-ui/src/app/students/student.service.ts
@@ -1,3 +1,4 @@
+import { AddStudentRequest } from './../models/api-models/add-student-request.model';
 import { UpdateStudentRequest } from './../models/api-models/update-student-request.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -29,7 +30,7 @@ export class StudentService {
     studentId: string,
     studentRequest: Student
   ): Observable<Student> {
-    const UpdateStudentRequest: UpdateStudentRequest = {
+    const updateStudentRequest: UpdateStudentRequest = {
       firstName: studentRequest.firstName,
       lastName: studentRequest.lastName,
       dateOfBirth: studentRequest.dateOfBirth,
@@ -43,7 +44,7 @@ export class StudentService {
     // return a type of student
     return this.httpsClient.put<Student>(
       this.baseApiUrl + '/students/' + studentId,
-      UpdateStudentRequest
+      updateStudentRequest
     );
   }
 
@@ -51,6 +52,30 @@ export class StudentService {
   deleteStudent(studentId: string): Observable<Student> {
     return this.httpsClient.delete<Student>(
       this.baseApiUrl + '/students/' + studentId
+    );
+  }
+
+  // this will receive an object request model of type student
+  // this will return Observable of type Student
+  //source
+
+  addStudent(studentRequest: Student): Observable<Student> {
+    //destination
+    const addStudentRequest: AddStudentRequest = {
+      firstName: studentRequest.firstName,
+      lastName: studentRequest.lastName,
+      dateOfBirth: studentRequest.dateOfBirth,
+      email: studentRequest.email,
+      mobile: studentRequest.mobile,
+      genderId: studentRequest.genderId,
+      physicalAddress: studentRequest.address.physicalAddress,
+      postalAddress: studentRequest.address.postalAddress,
+    };
+
+    //in return we are getting a response type of Student Class which is of API Student
+    return this.httpsClient.post<Student>(
+      this.baseApiUrl + '/students/add',
+      addStudentRequest
     );
   }
 }
